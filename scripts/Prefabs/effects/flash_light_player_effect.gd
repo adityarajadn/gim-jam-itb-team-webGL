d2829cc11c7bd2837968bf8cb6d9581308486641
HalfVizsh <muhmark246@gmail.com>
2026-01-28 15:21:00 +0700

    revert

--- scripts/Prefabs/effects/flash_light_player_effect.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@onready var fog: Sprite2D = $"../../fog"
-
-func flashLightEffect():
-	var default = fog.modulate.a
-	fog.modulate.a = 0.5
-	await get_tree().create_timer(5).timeout
-	fog.modulate.a = default
