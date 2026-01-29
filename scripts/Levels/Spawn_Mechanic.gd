6b09cb58e64a998833b96d9079b5bb32f0ba9e24
HalfVizsh <muhmark246@gmail.com>
2026-01-29 19:58:04 +0700

    Adding Wave System
    Adding Wavemanager Scene, make sure to fit all stuff to inspector first

--- scripts/Levels/Spawn_Mechanic.gd
+++ scripts/Levels/Spawn_Mechanic.gd
@@ -8,10 +8,12 @@ extends Node
 @export var regular_enemy: PackedScene
 @export var ranged_enemy: PackedScene
 @export var tank_enemy: PackedScene
-@export var EnemyPool : Array [PackedScene]
-
+var EnemyPool : Array [PackedScene]
 var spawn_timer := 0.0
 
+func _ready() -> void:
+	var player := get_tree().get_first_node_in_group("player")
+	
 func get_random_spawn_position() -> Vector2:
 	var angle = randf() * TAU #TAU biar jadi lingkaran
 	var distance = randf_range(min_radius, max_radius)
@@ -24,7 +26,7 @@ func spawn_enemy():
 		return
 	var enemy = EnemyPool.pick_random().instantiate()
 	enemy.global_position = get_random_spawn_position()
-	get_parent().add_child(enemy)
+	add_child(enemy)
 
 func get_enemy_pool_for_wave(wave: int) -> Array[PackedScene]:
 	var pool: Array[PackedScene] = []
@@ -36,15 +38,10 @@ func get_enemy_pool_for_wave(wave: int) -> Array[PackedScene]:
 		pool.append(tank_enemy)
 	return pool
 
-func spawn_wave_burst(count: int):
-	for i in count:
-		spawn_enemy()
-
-func _ready() -> void:
-	spawn_wave_burst(randf_range(2,6)) 
-
 func _process(delta: float) -> void:
-	spawn_timer += delta
-	if spawn_timer >= spawn_interval:
-		spawn_timer = 0
-		spawn_enemy()
+	var canSpawn = not wave_manager.in_prep
+	if (canSpawn):
+		spawn_timer += delta
+		if spawn_timer >= spawn_interval:
+			spawn_timer = 0
+			spawn_enemy()
